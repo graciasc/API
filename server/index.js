a372091a0cce49e07ed35e22205047666519a9d0
Graciasc <gracias.claude@hotmail.com>
2019-12-08 09:09:46 -0700

    added a data.json and a get request to get the data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const data =  require('./data.json');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT ||8000;
 
-app.use('/', (req, res) => {
-    res.send('Express');
+app.get('/', (req, res) => {
+    res.send('Node/Express API');
+})
+
+// can call this data when local host is running using fetch
+app.get('/data', (req, res) => {
+    res.send(data)
 })
 
 app.listen(port)
